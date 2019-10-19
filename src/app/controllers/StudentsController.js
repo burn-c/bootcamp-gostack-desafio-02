e08cbca7859fb34b81d294dcca2e8d8142eec887
burn-c <chopcsom@gmail.com>
2019-10-19 20:12:12 -0300

    Criação Controller->Students

---
+++ src/app/controllers/StudentsController.js
@@ -0,0 +1,22 @@
+import Students from '../models/Students';
+
+class StudentsController {
+  async store(req, res) {
+    const studentExists = await Students.findOne({
+      where: { email: req.body.email }
+    });
+
+    if (studentExists) {
+      return res.status(400).json({ error: 'Estudante já cadastrado!' });
+    }
+
+    const { name, email } = await Students.create(req.body);
+
+    return res.json({
+      name,
+      email
+    });
+  }
+}
+
+export default new StudentsController();
--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,9 @@
 import { Router } from 'express';
-import Students from './app/models/Students';
+
+import StudentsController from './app/controllers/StudentsController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const student = await Students.create({
-    name: 'Carlos Oliveira',
-    email: 'carlos@gym.com.br',
-    idade: '28',
-    peso: '60',
-    altura: '1.75',
-    password_hash: '123456'
-  });
-  return res.json(student);
-});
+routes.post('/students', StudentsController.store);
 
 export default routes;
